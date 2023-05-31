b3b1d39459f932d028d69e3c33e037dc31387768
Sokol Paja <kolpaja28@gmail.com>
2023-05-31 21:13:11 +0200

    now

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,49 @@
-export default [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      header: "*",
+      origin: ["*"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "https://cwk-startup.onrender.com",
+            "https://strapi-7rig.onrender.com",
+            "localhost:1338",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "https://cwk-startup.onrender.com",
+            "https://strapi-7rig.onrender.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
