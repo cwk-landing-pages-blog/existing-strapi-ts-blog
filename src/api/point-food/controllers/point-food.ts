d480fbb1407afd8d88b63c26e4080c880c7d6b41
Sokol Paja <kolpaja28@gmail.com>
2023-06-01 02:55:09 +0200

    test internalization

---
+++ src/api/point-food/controllers/point-food.ts
@@ -0,0 +1,7 @@
+/**
+ * point-food controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::point-food.point-food');
---
+++ src/api/point-food/routes/point-food.ts
@@ -0,0 +1,7 @@
+/**
+ * point-food router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::point-food.point-food');
