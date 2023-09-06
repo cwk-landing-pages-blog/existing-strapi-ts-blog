a0e684ccc518ce65c53b10499426dffcaf8d79d1
Sokol Paja <kolpaja28@gmail.com>
2023-09-06 10:14:21 +0200

    temp commit

---
+++ src/api/natural-stones-user/controllers/natural-stones-user.ts
@@ -0,0 +1,7 @@
+/**
+ * natural-stones-user controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::natural-stones-user.natural-stones-user');
---
+++ src/api/natural-stones-user/routes/natural-stones-user.ts
@@ -0,0 +1,7 @@
+/**
+ * natural-stones-user router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::natural-stones-user.natural-stones-user');
